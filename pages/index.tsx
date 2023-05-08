8aa70bb80c9cbe44706b4ed08e861ee221ef8e94
David <daivdpro@tuta.io>
2023-05-08 14:07:00 +0200

    fetchNFtOK

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,15 @@
 import Head from "next/head"
 import { useQuery } from "@tanstack/react-query"
 
+import { alchemy } from "@/lib/alchemy"
 import { Loading } from "@/components/Loading"
 import { Layout } from "@/components/layout"
 import { Card } from "@/components/ui/Card"
 
 export default function IndexPage() {
-  const { isLoading, error, data } = useQuery({
-    queryKey: ["coinprices"],
-    queryFn: () =>
-      fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false
-      `).then((res) => res.json()),
-  })
+  const { isLoading, error, data } = useQuery(["getNfts"], () =>
+    alchemy.nft.getNftsForContract("0xB003ce92F3b2A8F3dd99207C351eAf05BC605262")
+  )
 
   if (isLoading)
     return (
@@ -25,7 +23,7 @@ export default function IndexPage() {
   return (
     <Layout>
       <Head>
-        <title>Chainxplorer</title>
+        <title>Ternoa test</title>
         <meta
           name="description"
           content="Chainxplorer gives you the latest informations about crypto prices, ethereum blocks, and addresses content"
@@ -34,19 +32,7 @@ export default function IndexPage() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <section className="container grid flex-col justify-center  gap-6 pt-6 pb-8 md:grid-cols-2 md:py-10 lg:grid-cols-3">
-        {data.map((coin) => {
-          return (
-            <Card
-              id={coin.id}
-              current_price={coin.current_price}
-              name={coin.name}
-              key={coin.id}
-              ath={coin.ath}
-              image={coin.image}
-              max_supply={coin.max_supply}
-            ></Card>
-          )
-        })}
+        {console.log(data)}
       </section>
     </Layout>
   )
