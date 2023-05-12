782082c179a8c1c91116f5ef953c304785788f43
Dav-sa <68847199+Dav-sa@users.noreply.github.com>
2023-05-12 15:51:02 +0200

    Create test.cy.js

---
+++ tests/test.cy.js
@@ -0,0 +1,22 @@
+
+
+describe("ternoa test app", () => {
+  beforeEach(() => {
+    cy.visit("https://localhost:3000")
+  })
+
+  describe("Navigation", () => {
+    it("should navigate to the likes page", () => {
+      cy.get('a[href*="likes"]').click()
+
+      cy.url().should("include", "/likes")
+    })
+  })
+
+  describe("Likes", () => {
+    it("should display the title and likes", () => {
+      cy.get("p").contains("Title")
+      cy.get("p").contains("Liked by :")
+    })
+  })
+})
